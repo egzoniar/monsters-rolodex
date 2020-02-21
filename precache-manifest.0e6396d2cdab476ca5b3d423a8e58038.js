0476523e57758a4f56bd5f9555bd2d85d48ec25b
Egzon Arifaj <egzon080996@gmail.com>
2020-02-21 14:52:53 +0100

    Updates

---
+++ precache-manifest.0e6396d2cdab476ca5b3d423a8e58038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c6a9d473df6667ecb6aa2f1bc39a0d8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "45c6ec663b5a2fb03b45",
+    "url": "/monsters-rolodex/static/css/main.ff4c594d.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45c6ec663b5a2fb03b45",
+    "url": "/monsters-rolodex/static/js/main.5c73d51b.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
